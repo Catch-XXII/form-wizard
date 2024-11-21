8b11b320295ece15c7bbb43f78eb91700e4315c4
Cüneyd Gültekin Kaya <cgultekink@gmail.com>
2024-11-21 23:07:01 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 pip list
-SET ENV=C:\Users\cuneyd.kaya\dev\WebUIAutomation
+SET ENV=YOUR_PATH_TO_PROJECT_WITHOUT_QUOTES
 call %ENV%\.venv\Scripts\activate.bat
 pytest
