a8f6e88033786f1445a2c2942481a0625aca9d62
Cuneyd Kaya <cgultekink@gmail.com>
2024-11-23 13:19:05 +0300

    slo-mo param was removed from conftest

--- conftest.py
+++ conftest.py
@@ -7,7 +7,6 @@ def browser():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(
             headless=False,
-            slow_mo=750,
         )
         context = browser.new_context(viewport={"width": 1920, "height": 1200})
         page = context.new_page()
